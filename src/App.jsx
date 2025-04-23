242700fe38c2df87f2ee182adfc1700fa3d4d31c
Rishav Chandra <146986198+ScraperCharlie@users.noreply.github.com>
2025-04-23 23:57:38 +0530

    Component Changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import "./App.css";
 import Home from "./components/Home";
 import About from "./components/About";
